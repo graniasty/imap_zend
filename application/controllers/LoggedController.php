e61c7a7ea90d2ae8b7f36b00e03767ca391b5ca8
root <root@darek.(none)>
2015-12-15 10:54:46 -0500

    source server adding done

--- application/controllers/LoggedController.php
+++ application/controllers/LoggedController.php
@@ -14,25 +14,25 @@ class LoggedController extends Zend_Controller_Action {
     }
 
     public function indexAction() {
-        $this->view->menu_display = "none";
+        $this->_redirect('logged/transfer');
         //cos tam
     }
 
-   public function logoutAction() {
-       $storage = new Zend_Auth_Storage_Session();
-       $storage->clear();
-       $this->_redirect('auth/index');
-   }
-   
-   public function transferAction() {
-       $this->view->button_disabled = "disabled";
+    public function logoutAction() {
+        $storage = new Zend_Auth_Storage_Session();
+        $storage->clear();
+        $this->_redirect('auth/index');
+    }
+
+    public function transferAction() {
+        $this->view->button_disabled = "disabled";
         $sess = Zend_Registry::get('session');
         $form = new Application_Form_Source();
         $this->view->form = $form;
         $form2 = new Application_Form_Target();
         $this->view->form2 = $form2;
         $this->view->table_display = "none";
-        
+
 
         if ($this->getRequest()->isPost()) {
 
@@ -41,19 +41,34 @@ class LoggedController extends Zend_Controller_Action {
             if ($formData['action'] == 'source') {
                 if ($form->isValid($formData)) {
                     $this->view->table_display = "block";
+                    $this->view->form_display = "none";
                     $this->view->button_disabled = "disabled";
 
+                    $direction = 'source';
                     $host1 = $formData['host1'];
                     $user1 = $formData['user1'];
                     $password1 = $formData['password1'];
+                    $server = array($direction, $host1, $user1, $password1, 1);
 
-                    
+                    $sess = Zend_Registry::get('session');
+                    $t_servers = $sess->t_servers;
+                    $t_servers[1] = $server;
+                    $sess->t_servers = $t_servers;
+
+                    $this->view->t_servers = $sess->t_servers;
+
+                    if (strlen($t_servers[0][1]) > 0) {
+                        $this->view->form2_display = "none";
+                        $this->view->button_disabled = "";
+                    }
+                    echo 'tablica';
+                    var_dump($sess->t_servers);
                 } else {
                     $form->populate($formData);
                 }
             } else {
                 if ($form2->isValid($formData)) {
-
+                    $this->view->form2_display = "none";
                     $this->view->table_display = "block";
                     $this->view->button_disabled = "disabled";
 
@@ -61,19 +76,25 @@ class LoggedController extends Zend_Controller_Action {
                     $user2 = $formData['user2'];
                     $password2 = $formData['password2'];
 
+                    $direction = 'target';
+                    $host1 = $formData['host2'];
+                    $user1 = $formData['user2'];
+                    $password1 = $formData['password2'];
+                    $server = array($direction, $host2, $user2, $password2, 0);
+
                     $sess = Zend_Registry::get('session');
-                    $sess->host2 = $host2;
-                    $sess->user2 = $user2;
-                    $sess->password2 = $password2;
+                    $t_servers = $sess->t_servers;
+                    $t_servers[0] = $server;
+                    $sess->t_servers = $t_servers;
 
+                    if (strlen($t_servers[1][1] > 0)) {
+                        $this->view->form_display = "none";
+                        $this->view->button_disabled = "";
+                    }
 
-                    $this->view->host1 = $sess->host1;
-                    $this->view->user1 = $sess->user1;
-                    $this->view->password1 = $sess->password1;
-
-                    $this->view->host2 = $host2;
-                    $this->view->user2 = $user2;
-                    $this->view->password2 = $password2;
+                    $this->view->t_servers = $sess->t_servers;
+                    echo 'tablica';
+                    var_dump($sess->t_servers);
                 } else {
                     $form2->populate($formData);
                 }
@@ -83,5 +104,36 @@ class LoggedController extends Zend_Controller_Action {
                 $this->view->button_disabled = "";
             }
         }
-   }
+    }
+
+    public function addAction() {
+
+        $form = new Application_Form_Source();
+        $this->view->form = $form;
+        $sess = Zend_Registry::get('session');
+        $this->view->t_servers = $sess->t_servers;
+
+        if ($this->getRequest()->isPost()) {
+
+            $formData = $this->getRequest()->getPost();
+
+            if ($form->isValid($formData)) {
+
+                $direction = 'source';
+                $host1 = $formData['host1'];
+                $user1 = $formData['user1'];
+                $password1 = $formData['password1'];
+                $server = array($direction, $host1, $user1, $password1, 1);
+
+                $sess = Zend_Registry::get('session');
+                $t_servers = $sess->t_servers;
+                $t_servers[] = $server;
+                $sess->t_servers = $t_servers;
+                $this->view->t_servers = $sess->t_servers;
+            } else {
+                $form->populate($formData);
+            }
+        }
+    }
+
 }
